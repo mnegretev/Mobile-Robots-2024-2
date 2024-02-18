fc1a9c21a9dad87725133a756fd614b52297ee65
Alan EGV <alanegv29@gmail.com>
2024-02-18 15:12:04 -0600

    Tarea 3

--- catkin_ws/src/navigation/map_augmenter/scripts/map_inflater.py
+++ catkin_ws/src/navigation/map_augmenter/scripts/map_inflater.py
@@ -28,7 +28,12 @@ def get_inflated_map(static_map, inflation_cells):
     # Map is given in 'static_map' as a bidimensional numpy array.
     # Consider as occupied cells all cells with an occupation value greater than 50
     #
-    
+    for i in range(len(static_map)):
+        for j in range(len(static_map[0])):
+            if(static_map[i,j] == 100):
+                for k1 in range(-inflation_cells, inflation_cells):
+                    for k2 in range(-inflation_cells, inflation_cells):
+                        inflated[i+k1,j+k2] = 100
     return inflated
 
 def callback_inflated_map(req):
