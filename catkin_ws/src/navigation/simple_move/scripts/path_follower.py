6a7849170ab612bea7a27818a68f4b620cd3e338
Th3ProHero <bautista.flores.mauricio2@gmail.com>
2024-02-21 20:39:38 -0600

    Tarea 6: Seguimiento de ruta en simulador

--- catkin_ws/src/navigation/simple_move/scripts/path_follower.py
+++ catkin_ws/src/navigation/simple_move/scripts/path_follower.py
@@ -27,41 +27,34 @@ loop        = None
 listener    = None
 
 def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y, alpha, beta, v_max, w_max):
-    v,w = 0,0
-    #
-    # TODO:
-    # Implement the control law given by:
-    #
-    # v = v_max*math.exp(-error_a*error_a/alpha)
-    # w = w_max*(2/(1 + math.exp(-error_a/beta)) - 1)
-    #
-    # where error_a is the angle error
-    # and v_max, w_max, alpha and beta, are tunning constants.
-    # Remember to keep error angle in the interval (-pi,pi]
-    # Return the tuple [v,w]
-    #
-        
-    return [v,w]
+    e_a = math.atan2(goal_y - robot_y, goal_x - robot_x) - robot_a
+    v = v_max * math.exp(-e_a**2 / alpha)
+    w = w_max * (2 / (1 + math.exp(-e_a / beta)) - 1)
 
+    # Asegurar que el ángulo de error esté en el intervalo (-pi, pi]
+    if w > math.pi:
+        w -= 2 * math.pi
+    elif w <= -math.pi:
+        w += 2 * math.pi
+
+    return [v, w]
+
+
+# Dentro de la función follow_path
 def follow_path(path, alpha, beta, v_max, w_max):
-    #
-    # TODO:
-    # Use the calculate_control function to move the robot along the path.
-    # Path is given as a sequence of points [[x0,y0], [x1,y1], ..., [xn,yn]]
-    # You can use the following steps to perform the path tracking:
-    #
-    # Set goal point as the first point of the path
-    # Get robot position with Pr, robot_a = get_robot_pose()
-    #
-    # WHILE distance to last point > tol and not rospy.is_shutdown() 
-    #     Calculate control signals v and w
-    #     Publish the control signals with the function publish_twist()
-    #     Get robot position
-    #     If dist to goal point is less than 0.3 (you can change this constant)
-    #         Change goal point to the next point in the path
-    #
-            
-    return
+    id_x = 0
+    Pr, robot_a = get_robot_pose()
+    Pg = path[id_x]
+
+    while numpy.linalg.norm(path[-1] - Pr) > 0.1 and id_x < len(path) - 1 and not rospy.is_shutdown():
+        v, w = calculate_control(Pr[0], Pr[1], robot_a, Pg[0], Pg[1], alpha, beta, v_max, w_max)
+        publish_twist(v, w)
+        Pr, robot_a = get_robot_pose()
+
+        if numpy.linalg.norm(Pg - Pr) < 0.3:
+            id_x = min(id_x + 1, len(path) - 1)
+            Pg = path[id_x]
+
         
 
 def publish_twist(v,w):
