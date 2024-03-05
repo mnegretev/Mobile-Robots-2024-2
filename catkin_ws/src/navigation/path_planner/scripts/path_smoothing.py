8bbcba4efa38798a15989196e684667287491620
emipina-f <54326897+emipina-f@users.noreply.github.com>
2024-03-05 13:58:18 -0600

    Tarea 7 emipina

--- catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
+++ catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
@@ -16,23 +16,23 @@ from geometry_msgs.msg import Pose, PoseStamped, Point
 from navig_msgs.srv import ProcessPath
 from navig_msgs.srv import ProcessPathResponse
 
-NAME = "FULL NAME"
+NAME = "Emilio Piña Felix"
 
 def smooth_path(Q, alpha, beta, max_steps):
-    #
-    # TODO:
-    # Write the code to smooth the path Q, using the gradient descend algorithm,
-    # and return a new smoothed path P.
-    # Path is composed of a set of points [x,y] as follows:
-    # [[x0,y0], [x1,y1], ..., [xn,ym]].
-    # The smoothed path must have the same shape.
-    # Return the smoothed path.
-    #
     P = numpy.copy(Q)
-    tol     = 0.00001                   
-    nabla   = numpy.full(Q.shape, float("inf"))
-    epsilon = 0.1                       
-    
+    tol = 0.00001
+    nabla = numpy.full(Q.shape, float("inf"))
+    epsilon = 0.1
+    steps = 0
+
+    while numpy.linalg.norm(nabla) > tol and steps < max_steps:
+        nabla = numpy.zeros(Q.shape)
+
+        for i in range(1, Q.shape[0] - 1):
+            nabla[i] = alpha * (2 * P[i] - P[i - 1] - P[i + 1]) + beta * (P[i] - Q[i])
+
+        P = P - epsilon * nabla
+        steps += 1                      
     
     return P
 
