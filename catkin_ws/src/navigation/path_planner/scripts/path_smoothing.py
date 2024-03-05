617e12d5c26131a2f995ac093004b3e52ce8bb2e
caleman4 <caleman4@robots2.myguest.virtualbox.org>
2024-03-05 14:09:09 -0600

    Entrega Tarea 7

--- catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
+++ catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
@@ -32,7 +32,17 @@ def smooth_path(Q, alpha, beta, max_steps):
     tol     = 0.00001                   
     nabla   = numpy.full(Q.shape, float("inf"))
     epsilon = 0.1                       
-    
+    steps = 0
+    nabla[0]=0
+    nabla[len(Q)-1]=0
+    i=0
+    print(P[i][0])
+    while numpy.any(abs(nabla)>tol) and steps<max_steps:
+    	for i in range (1,len(Q)-1):
+    		nabla[i]=(alpha*(2*P[i]-P[i-1]-P[i+1]))+(beta*(P[i]-Q[i]))
+    	P=P-epsilon*nabla
+    	steps=steps+1
+
     
     return P
 
