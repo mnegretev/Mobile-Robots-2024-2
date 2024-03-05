dbb912f84a76b65878b2015f384715b77f9a9d35
anafragoso2 <ana.fragoso@comunidad.unam.mx>
2024-03-05 13:39:06 -0600

    Correción de implementación en tarea 7

--- catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
+++ catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
@@ -33,15 +33,14 @@ def smooth_path(Q, alpha, beta, max_steps):
     nabla   = numpy.full(Q.shape, float("inf"))
     epsilon = 0.1  
     steps = 0
+    nabla[0] = 0
     nabla[len(Q) - 1] = 0
 
-    while numpy.any(abs(nabla)) > tol and steps < max_steps:
-        for i in range(1, len(Q) - 1):
-            nabla[i] = alpha*((2*P[i]) - P[i-1] - P[i+1]) + beta*(P[i] - Q[i])   
+    while numpy.any(abs(nabla) > tol) and steps < max_steps:
+        for i in range(1, len(Q)-1):
+            nabla[i] = alpha*(2*P[i]-P[i+1]) + beta*(P[i]-Q[i]) 
         P = P - epsilon*nabla
         steps = steps + 1              
-    
-    
     return P
 
 def callback_smooth_path(req):
