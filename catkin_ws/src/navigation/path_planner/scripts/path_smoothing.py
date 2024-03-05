ab7666a702b5bc7357cb922556aa3c709dfcdc37
Gadiel Fonseca <igadu_99@outlook.com>
2024-03-05 14:59:49 -0600

    tarea 07 suavizado de rutas

--- catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
+++ catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
@@ -28,11 +28,20 @@ def smooth_path(Q, alpha, beta, max_steps):
     # The smoothed path must have the same shape.
     # Return the smoothed path.
     #
+    
     P = numpy.copy(Q)
     tol     = 0.00001                   
     nabla   = numpy.full(Q.shape, float("inf"))
-    epsilon = 0.1                       
+    epsilon = 0.1
     
+    steps = 0
+    nabla [0] = 0
+    nabla [-1] = 0
+    while numpy.linalg.norm(nabla) > tol and steps < max_steps:
+        for i in range(1, len(Q) - 1):
+            nabla[i] = alpha * (2 * P[i] - P[i - 1] - P[i + 1]) + beta * (P[i] - Q[i])
+    P = P - (epsilon * nabla)
+    steps += 1          
     
     return P
 
