27e5c9754baf7cdbf8312a60c752a8dd731d249c
Eliab Carmona <damianeliabs@gmail.com>
2024-03-04 20:14:33 -0800

    Tarea 7 - implementacion descenso del gradeinte

--- catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
+++ catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
@@ -16,7 +16,7 @@ from geometry_msgs.msg import Pose, PoseStamped, Point
 from navig_msgs.srv import ProcessPath
 from navig_msgs.srv import ProcessPathResponse
 
-NAME = "FULL NAME"
+NAME = "Carmona Lopez DAmian Eliab"
 
 def smooth_path(Q, alpha, beta, max_steps):
     #
@@ -31,9 +31,15 @@ def smooth_path(Q, alpha, beta, max_steps):
     P = numpy.copy(Q)
     tol     = 0.00001                   
     nabla   = numpy.full(Q.shape, float("inf"))
-    epsilon = 0.1                       
-    
-    
+    epsilon = 0.1   
+                        
+    steps=0
+    nabla[0],nabla[-1]=0,0
+    while numpy.linalg.norm(nabla) > tol and steps < max_steps:
+        for i in range (1, len(Q)-1):
+            nabla[i]=alpha*(2*P[i]-P[i-1]-P[i+1])+ beta*(P[i]-Q[i])
+        P=P-epsilon*nabla
+        steps+=1
     return P
 
 def callback_smooth_path(req):
