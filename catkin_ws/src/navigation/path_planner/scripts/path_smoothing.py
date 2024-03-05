fe8f77b0561078da4fc99c9cbed7b19cc8b94adc
casstrosa4 <cass.alva4@gmail.com>
2024-03-05 01:56:34 -0600

    Se agrega un suavizado de rutas, tanto se comprueba como esta antes de hacerlo, còmo despuès

--- catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
+++ catkin_ws/src/navigation/path_planner/scripts/path_smoothing.py
@@ -16,7 +16,7 @@ from geometry_msgs.msg import Pose, PoseStamped, Point
 from navig_msgs.srv import ProcessPath
 from navig_msgs.srv import ProcessPathResponse
 
-NAME = "FULL NAME"
+NAME = "Casandra Itzel Àlvarez Sànchez"
 
 def smooth_path(Q, alpha, beta, max_steps):
     #
@@ -31,9 +31,16 @@ def smooth_path(Q, alpha, beta, max_steps):
     P = numpy.copy(Q)
     tol     = 0.00001                   
     nabla   = numpy.full(Q.shape, float("inf"))
-    epsilon = 0.1                       
+    epsilon = 0.1
+    steps = 0                       
+    nabla[0]= 0
+    nabla[len(Q)-1]= 0
     
-    
+    while numpy.any(abs(nabla) > tol) and steps < max_steps:
+    	for i in range (1,len(Q)-1):
+    		nabla[i]=alpha*(2*P[i]-P[i+1]) + beta*(P[i]-Q[i])
+    	P = P - epsilon*nabla
+    	steps = steps+1
     return P
 
 def callback_smooth_path(req):
