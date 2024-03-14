6b14b41b126bd699b968aeee7c28a46f194587e1
Gadiel Fonseca <igadu_99@outlook.com>
2024-03-14 15:37:22 -0600

    tarea 08 ok

--- catkin_ws/src/navigation/path_planner/scripts/pot_fields.py
+++ catkin_ws/src/navigation/path_planner/scripts/pot_fields.py
@@ -17,7 +17,7 @@ from geometry_msgs.msg import Twist, PoseStamped, Point, Vector3
 from visualization_msgs.msg import Marker
 from sensor_msgs.msg import LaserScan
 
-listener    = None
+listener = None
 pub_cmd_vel = None
 pub_markers = None
 laser_readings = None
@@ -26,8 +26,10 @@ w_max = 1.0
 
 NAME = "FULL NAME"
 
+
 def calculate_control(goal_x, goal_y, alpha, beta):
-    v,w = 0,0
+    v, w = 0, 0
+    robot_x, robot_y, robot_a = 0, 0, 0
     #
     # TODO:
     # Implement the control law given by:
@@ -35,11 +37,21 @@ def calculate_control(goal_x, goal_y, alpha, beta):
     # w = w_max*(2/(1 + math.exp(-error_a/beta)) - 1)
     # Return v and w as a tuble [v,w]
     #    
-    
-    return [v,w]
+
+    # Error angle
+    error_a = math.atan2(goal_y - robot_y, goal_x - robot_x) - robot_a
+    # Remember to keep error angle in the interval (-pi,pi]
+    if (error_a <= -math.pi) or (error_a > math.pi):
+        error_a = ((error_a + math.pi) / (2 * math.pi)) - math.pi
+    # Note. v_max, w_max, alpha and beta, are tunning constants.
+    v = v_max * math.exp(-(error_a * error_a) / alpha)
+    w = w_max * ((2 / (1 + math.exp(-error_a / beta))) - 1)
+
+    return [v, w]
+
 
 def attraction_force(goal_x, goal_y, eta):
-    force_x, force_y = 0,0
+    force_x, force_y = 0, 0
     #
     # TODO:
     # Calculate the attraction force, given the robot and goal positions.
@@ -47,8 +59,12 @@ def attraction_force(goal_x, goal_y, eta):
     # where force_x and force_y are the X and Y components
     # of the resulting attraction force
     #
-    
+
+    force_x = -eta * (goal_x / numpy.linalg.norm([goal_x, goal_y]))
+    force_y = -eta * (goal_y / numpy.linalg.norm([goal_x, goal_y]))
+
     return numpy.asarray([force_x, force_y])
+
 
 def rejection_force(laser_readings, zeta, d0):
     N = len(laser_readings)
@@ -66,41 +82,62 @@ def rejection_force(laser_readings, zeta, d0):
     # where force_x and force_y are the X and Y components
     # of the resulting rejection force
     #
-    
-        
+    for d, theta in laser_readings:
+        if d < d0:
+            rho = zeta * numpy.sqrt((1/d) - (1/d0))
+        else:
+            rho = 0
+        force_x = force_x + (rho * numpy.cos(theta))
+        force_y = force_x + (rho * numpy.sin(theta))
+    force_x = force_x / N
+    force_y = force_y / N
+
     return numpy.asarray([force_x, force_y])
+
 
 def move_by_pot_fields(global_goal_x, global_goal_y, epsilon, tol, eta, zeta, d0, alpha, beta):
     #
     # TODO
     # Implement potential fields given a goal point and tunning constants 
     #
-    
+    goal_x, goal_y = get_goal_point_wrt_robot(global_goal_x, global_goal_y)
+    while math.sqrt(goal_x ** 2 + goal_y ** 2) > tol and not rospy.is_shutdown():
+        Fa = attraction_force(goal_x, goal_y, eta)
+        Fr = rejection_force(laser_readings, zeta, d0)
+        F = Fa + Fr
+        P = -epsilon * F
+        [v, w] = calculate_control(P[0], P[1], alpha, beta)
+        publish_speed_and_forces(v, w, Fa, Fr, F)
+        goal_x, goal_y = get_goal_point_wrt_robot(global_goal_x, global_goal_y)
+
     return
-        
+
 
 def get_goal_point_wrt_robot(goal_x, goal_y):
     robot_x, robot_y, robot_a = get_robot_pose(listener)
     delta_x = goal_x - robot_x
     delta_y = goal_y - robot_y
-    goal_x =  delta_x*math.cos(robot_a) + delta_y*math.sin(robot_a)
-    goal_y = -delta_x*math.sin(robot_a) + delta_y*math.cos(robot_a)
+    goal_x = delta_x * math.cos(robot_a) + delta_y * math.sin(robot_a)
+    goal_y = -delta_x * math.sin(robot_a) + delta_y * math.cos(robot_a)
     return [goal_x, goal_y]
+
 
 def get_robot_pose(listener):
     try:
-        ([x, y, z], [qx,qy,qz,qw]) = listener.lookupTransform('map', 'base_link', rospy.Time(0))
-        return [x, y, 2*math.atan2(qz, qw)]
+        ([x, y, z], [qx, qy, qz, qw]) = listener.lookupTransform('map', 'base_link', rospy.Time(0))
+        return [x, y, 2 * math.atan2(qz, qw)]
     except:
-        return [0,0,0]
+        return [0, 0, 0]
+
 
 def publish_speed_and_forces(v, w, Fa, Fr, F):
     loop = rospy.Rate(20)
     pub_cmd_vel.publish(Twist(linear=Vector3(x=v), angular=Vector3(z=w)))
     pub_markers.publish(get_force_marker(Fa[0], Fa[1], [0.0, 0.0, 1.0, 1.0], 0))
     pub_markers.publish(get_force_marker(Fr[0], Fr[1], [1.0, 0.0, 0.0, 1.0], 1))
-    pub_markers.publish(get_force_marker(F [0], F [1], [0.0, 0.6, 0.0, 1.0], 2))
+    pub_markers.publish(get_force_marker(F[0], F[1], [0.0, 0.6, 0.0, 1.0], 2))
     loop.sleep()
+
 
 def get_force_marker(force_x, force_y, color, id):
     mrk = Marker()
@@ -117,23 +154,26 @@ def get_force_marker(force_x, force_y, color, id):
     mrk.points.append(Point(x=-force_x, y=-force_y))
     return mrk
 
+
 def callback_scan(msg):
     global laser_readings
-    laser_readings = [[msg.ranges[i], msg.angle_min+i*msg.angle_increment] for i in range(len(msg.ranges))]
+    laser_readings = [[msg.ranges[i], msg.angle_min + i * msg.angle_increment] for i in range(len(msg.ranges))]
+
 
 def callback_pot_fields_goal(msg):
     [goal_x, goal_y] = [msg.pose.position.x, msg.pose.position.y]
-    print("Moving to goal point " + str([goal_x, goal_y]) + " by potential fields"    )
+    print("Moving to goal point " + str([goal_x, goal_y]) + " by potential fields")
     epsilon = rospy.get_param('~epsilon', 0.5)
-    tol     = rospy.get_param('~tol', 0.5)
-    eta     = rospy.get_param('~eta', 2.0)
-    zeta    = rospy.get_param('~zeta', 6.0)
-    d0      = rospy.get_param('~d0', 1.0)
-    alpha   = rospy.get_param('~alpha', 0.5)
-    beta    = rospy.get_param('~beta', 0.5)
+    tol = rospy.get_param('~tol', 0.5)
+    eta = rospy.get_param('~eta', 2.0)
+    zeta = rospy.get_param('~zeta', 6.0)
+    d0 = rospy.get_param('~d0', 1.0)
+    alpha = rospy.get_param('~alpha', 0.5)
+    beta = rospy.get_param('~beta', 0.5)
     move_by_pot_fields(goal_x, goal_y, epsilon, tol, eta, zeta, d0, alpha, beta)
     pub_cmd_vel.publish(Twist())
     print("Global goal point reached")
+
 
 def main():
     global listener, pub_cmd_vel, pub_markers
@@ -141,14 +181,15 @@ def main():
     rospy.init_node("pot_fields")
     rospy.Subscriber("/hardware/scan", LaserScan, callback_scan)
     rospy.Subscriber('/move_base_simple/goal', PoseStamped, callback_pot_fields_goal)
-    pub_cmd_vel = rospy.Publisher('/cmd_vel', Twist,  queue_size=10)
+    pub_cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     pub_markers = rospy.Publisher('/navigation/pot_field_markers', Marker, queue_size=10)
     listener = tf.TransformListener()
     rospy.spin()
+
 
 if __name__ == '__main__':
     try:
         main()
     except rospy.ROSInterruptException:
         pass
-    
+
